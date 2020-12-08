c8b5e3f7a3f38cdb09998b308d4bbd30fb355ffe
Justin Suderman <jsuderman1@gmail.com>
2020-12-08 14:33:29 -0800

    added routes and local api

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 //requireing
+const bodyParser = require('body-parser')
 const express = require('express');
 const mongoose = require('mongoose');
-
+const Message = require('./dbMessages')
 //app config
 
 const app = express();
 const port = process.env.PORT || 8000
 //middleware
+
+app.use(bodyParser.json());
+app.use(express.json());
 
 
 // DB config
@@ -15,15 +19,38 @@ const connection_url = 'mongodb+srv://admin:7kRKsplLoa5X7JhB@cluster0.ibs7n.mong
 mongoose.connect(connection_url, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+    
+});
 
 // ????
 
 
 // api routes
 
-app.get('/', (req, res) => res.status(200).send('Hello world'))
+app.get('/', (req, res) => res.status(200).send('Hello world'));
+
+app.get('/messages/sync', (req, res) => {
+    Message.find((err,data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body
+
+    Message.create(dbMessage, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
 
 // ;listener
 app.listen(port, () => console.log(`listening on localhost:${port}`))
