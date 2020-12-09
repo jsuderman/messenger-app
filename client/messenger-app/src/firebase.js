1cb626c81c9d55878aa29cd32214749067db8f8c
Justin Suderman <jsuderman1@gmail.com>
2020-12-09 11:47:05 -0800

    set up firebase for auth

--- client/messenger-app/src/firebase.js
+++ client/messenger-app/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBZ0YAg2vrE3u97YoxYFibgWq_PvNCZ4tg",
     authDomain: "messenger-app-1c5f2.firebaseapp.com",
@@ -6,3 +8,12 @@ const firebaseConfig = {
     messagingSenderId: "876520428080",
     appId: "1:876520428080:web:c32c7013916728e49953f5"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
