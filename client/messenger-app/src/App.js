1b6e56ac1bf5aee2a1d8bc91a7272c87f82a9158
Justin Suderman <jsuderman1@gmail.com>
2020-12-08 18:39:30 -0800

    updated app.js with pusher functionality to pull from backend and display on frontend

--- client/messenger-app/src/App.js
+++ client/messenger-app/src/App.js
@@ -1,17 +1,45 @@
 import './App.css';
 import Sidebar from "./components/Sidebar/Sidebar";
 import Chat from "./components/Chat/Chat";
-
+import Pusher from "pusher-js";
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+      axios.get('/messages/sync').then(response => {
+        console.log(response.data);
+        setMessages(response.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    const pusher = new Pusher('e49696d1456791ae2abf', {
+      cluster: 'us3'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      // alert(JSON.stringify(data));
+      setMessages([...messages, data])
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
       <Sidebar />
-      <Chat />
-      
-
-
+      <Chat messages={messages}/>
       </div>
     </div>
   );
